1365daa57a07d077ca8a8d57022e1e3604584b77
harshithakenaje <harshitha.ks@techjini.com>
2018-06-13 12:28:01 +0530

    use fork for background process

--- server/public/root/childprocess.js
+++ server/public/root/childprocess.js
@@ -1,11 +1,22 @@
-const spawn = require('child_process').spawn;
+const childp = require('child_process');
+var forkchild = childp.fork('./server/public/root/worker');
 if(process.argv[2]=='child'){
     console.log("I am inside child");
 }else{
     console.log(__filename);
     var filename ='/home/techjini/Desktop/nodejs/bookApp/server/server.js'
-    var child = spawn(process.execPath,[filename]);
+    var child = childp.spawn(process.execPath,[filename]);
     child.stdout.on('data',function (data){
         console.log('from the child',data.toString());
-    })
+    });
+
+    
+//fork a child and doesn't wait for spawn process to complete - run in background
+forkchild.on('message', function(m) {
+  // Receive results from child process
+  console.log('received: ' + m);
+});
+
+// Send child process some work
+forkchild.send('Please up-case this string');
 }
---
+++ server/public/root/worker.js
@@ -0,0 +1,8 @@
+process.on('message', function(m) {
+    // Do work  (in this case just up-case the string
+    m = m.toUpperCase();
+  
+    // Pass results back to parent process
+    process.send(m.toUpperCase(m));
+  });
+  
